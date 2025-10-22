7548bfe002792f001b5b9697b142560434438419
Franco Ghilardi <franco.ghilardi1@gmail.com>
2025-10-22 12:06:05 -0300

    fix: correcion de fecha de alta

--- 03_carga_masiva.sql
+++ 03_carga_masiva.sql
@@ -6,6 +6,11 @@ SET SESSION cte_max_recursion_depth = 100000;
 -- Cantidad de legajos a generar
 SET @N := 100000;
 
+-- Parámetros de fecha
+SET @START_DATE := DATE('2015-01-01');
+SET @SPAN_LEGAJO_DAYS := DATEDIFF(CURDATE(), @START_DATE); 
+
+
 -- Transacción (se insertan ambas tablas, o ninguna)
 START TRANSACTION;
 
@@ -26,7 +31,7 @@ SELECT
   CONCAT('LEG-', LPAD(n, 6, '0')) AS nroLegajo,                  
   ELT(1 + (n % 5), 'JR', 'SSR', 'SR', 'LEAD', 'TRAINEE') AS categoria, 
   ELT(1 + (n % 2), 'ACTIVO', 'INACTIVO') AS estado,              
-  DATE_ADD('2015-01-01', INTERVAL FLOOR(RAND() * 4000) DAY) AS fechaAlta,
+  DATE_ADD(@START_DATE, INTERVAL FLOOR(RAND() * (@SPAN_LEGAJO_DAYS + 1)) DAY) AS fechaAlta,
   NULL AS observaciones                                          
 FROM seq;
 
@@ -42,7 +47,10 @@ SELECT
   t.apellido,                                                   
   CONCAT('D', LPAD(t.legajo_id, 9, '0')) AS dni,                 
   LOWER(CONCAT(LEFT(t.nombre, 1), '.', t.apellido_email, t.legajo_id, '@empresa.test')) AS email, 
-  DATE_ADD(t.fechaAlta, INTERVAL FLOOR(RAND() * 900) DAY) AS fechaIngreso, 
+   DATE_ADD(
+    t.fechaAlta,
+    INTERVAL FLOOR(RAND() * (GREATEST(DATEDIFF(CURDATE(), t.fechaAlta), 0) + 1)) DAY
+  ) AS fechaIngreso,
   t.area,                                                        
   t.legajo_id AS legajo                                          
 FROM (
